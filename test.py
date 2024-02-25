18625e1e6a3856e51a3a582510d3a07ea0eb0898
Aaryan Garg <aaryan.garg1@gmail.com>
2024-02-25 19:02:09 +0530

    ft : configuring dist training

--- test.py
+++ test.py
@@ -101,7 +101,7 @@ def build_model_main(args):
 def main(args):
     
 
-    # utils.setup_distributed(args)
+    utils.setup_distributed(args)
     # load cfg file and update the args
     print("Loading config file from {}".format(args.config_file))
     time.sleep(args.rank * 0.02)
@@ -180,46 +180,25 @@ def main(args):
         model._set_static_graph()
         model_without_ddp = model.module
 
+    if args.distributed :
+        effvit_backbone = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu], find_unused_parameters=args.find_unused_params)
+        effvit_backbone = effvit_backbone.module
+    
     logger.debug("build dataset ... ...")
-    if not args.eval:
-        num_of_dataset_train = len(dataset_meta["train"])
-        if num_of_dataset_train == 1:
-            dataset_train = bbuild_dataset(image_set='train', args=args, datasetinfo=dataset_meta["train"][0])
-        else:
-            from torch.utils.data import ConcatDataset
-            dataset_train_list = []
-            for idx in range(len(dataset_meta["train"])):
-                dataset_train_list.append(bbuild_dataset(image_set='train', args=args, datasetinfo=dataset_meta["train"][idx]))
-            dataset_train = ConcatDataset(dataset_train_list)
-        logger.debug("build dataset, done.")
-        logger.debug(f'number of training dataset: {num_of_dataset_train}, samples: {len(dataset_train)}')
-
+    dataset_train = bbuild_dataset(image_set='train', args=args, datasetinfo=dataset_meta["train"][0])
     dataset_val = bbuild_dataset(image_set='val', args=args, datasetinfo=dataset_meta["val"][0])
+    logger.debug("build dataset, done.")
 
     if args.distributed:
         sampler_val = DistributedSampler(dataset_val, shuffle=False)
-        if not args.eval:
-            sampler_train = DistributedSampler(dataset_train)
+        sampler_train = DistributedSampler(dataset_train)
     else:
         sampler_val = torch.utils.data.SequentialSampler(dataset_val)
-        if not args.eval:
-            sampler_train = torch.utils.data.RandomSampler(dataset_train)
+        sampler_train = torch.utils.data.RandomSampler(dataset_train)
 
-    if not args.eval:
-        batch_sampler_train = torch.utils.data.BatchSampler(
-            sampler_train, args.batch_size, drop_last=True)
-        data_loader_train = DataLoader(dataset_train, batch_sampler=batch_sampler_train,
-                                    collate_fn=utils.collate_fn, num_workers=args.num_workers)
-
-    data_loader_val = DataLoader(dataset_val, 4, sampler=sampler_val,
-                                 drop_last=False, collate_fn=utils.collate_fn, num_workers=args.num_workers)
-
-    # if args.onecyclelr:
-    #     lr_scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=args.lr, steps_per_epoch=len(data_loader_train), epochs=args.epochs, pct_start=0.2)
-    # elif args.multi_step_lr:
-    #     lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=args.lr_drop_list)
-    # else:
-    #     lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, args.lr_drop)
+    batch_sampler_train = torch.utils.data.BatchSampler(sampler_train, args.batch_size, drop_last=True)
+    data_loader_train = DataLoader(dataset_train, batch_sampler=batch_sampler_train,collate_fn=utils.collate_fn, num_workers=args.num_workers)
+    data_loader_val = DataLoader(dataset_val, 4, sampler=sampler_val,drop_last=False, collate_fn=utils.collate_fn, num_workers=args.num_workers)
 
     trainer = GdinoBackboneTrainer(
         path=args.path,
@@ -229,15 +208,13 @@ def main(args):
         auto_restart_thresh=args.auto_restart_thresh,
     )
 
-    # initialization --> weight init (norm layers) etc
     setup.init_model(
         trainer.network,
         rand_init=args.rand_init,
         last_gamma=args.last_gamma,
     )
-    # prep for training
+
     trainer.prep_for_training_custom(run_config, config["ema_decay"], args.fp16)
-    print("Prep for training...")
 
     # Extract LR scheduler + Optimizer + scaler??
 
@@ -252,21 +229,8 @@ def main(args):
     output_dir = Path(args.output_dir)
     if os.path.exists(os.path.join(args.output_dir, 'checkpoint.pth')):
         args.resume = os.path.join(args.output_dir, 'checkpoint.pth')
-    if args.resume:
-        if args.resume.startswith('https'):
-            checkpoint = torch.hub.load_state_dict_from_url(
-                args.resume, map_location='cpu', check_hash=True)
-        else:
-            checkpoint = torch.load(args.resume, map_location='cpu')
-        model_without_ddp.load_state_dict(clean_state_dict(checkpoint['model']),strict=False)
 
-        
-        if not args.eval and 'optimizer' in checkpoint and 'lr_scheduler' in checkpoint and 'epoch' in checkpoint:
-            optimizer.load_state_dict(checkpoint['optimizer'])
-            lr_scheduler.load_state_dict(checkpoint['lr_scheduler'])
-            args.start_epoch = checkpoint['epoch'] + 1
-
-    if (not args.resume) and args.pretrain_model_path:
+    if  args.pretrain_model_path:
         print("loading original model weights")
         checkpoint = torch.load(args.pretrain_model_path, map_location='cpu')['model']
         from collections import OrderedDict
@@ -286,116 +250,22 @@ def main(args):
         _load_output = model_without_ddp.load_state_dict(_tmp_st, strict=False)
         logger.info(str(_load_output))
 
-    print("Starting eval")
     trainer.train(save_freq=args.save_freq)
  
     
-    if args.eval:
-        os.environ['EVAL_FLAG'] = 'TRUE'
-        test_stats, coco_evaluator = evaluate(model, criterion, postprocessors,
-                                              data_loader_val, base_ds, device, args.output_dir, wo_class_error=wo_class_error, args=args)
-        if args.output_dir:
-            utils.save_on_master(coco_evaluator.coco_eval["bbox"].eval, output_dir / "eval.pth")
-
-        log_stats = {**{f'test_{k}': v for k, v in test_stats.items()} }
-        if args.output_dir and utils.is_main_process():
-            with (output_dir / "log.txt").open("a") as f:
-                f.write(json.dumps(log_stats) + "\n")
-
-        return
-    
- 
-    
-    # print("Start training")
-    # start_time = time.time()
-    # best_map_holder = BestMetricHolder(use_ema=False)
-
-    # for epoch in range(args.start_epoch, args.epochs):
-    #     epoch_start_time = time.time()
-    #     if args.distributed:
-    #         sampler_train.set_epoch(epoch)
-
-        # train_stats = train_one_epoch(
-            # model, criterion, data_loader_train, optimizer, device, epoch,
-    #         args.clip_max_norm, wo_class_error=wo_class_error, lr_scheduler=lr_scheduler, args=args, logger=(logger if args.save_log else None))
+    # if args.eval:
+    #     os.environ['EVAL_FLAG'] = 'TRUE'
+    #     test_stats, coco_evaluator = evaluate(model, criterion, postprocessors,
+    #                                           data_loader_val, base_ds, device, args.output_dir, wo_class_error=wo_class_error, args=args)
     #     if args.output_dir:
-    #         checkpoint_paths = [output_dir / 'checkpoint.pth']
+    #         utils.save_on_master(coco_evaluator.coco_eval["bbox"].eval, output_dir / "eval.pth")
 
-    #     if not args.onecyclelr:
-    #         lr_scheduler.step()
-    #     if args.output_dir:
-    #         checkpoint_paths = [output_dir / 'checkpoint.pth']
-    #         # extra checkpoint before LR drop and every 100 epochs
-    #         if (epoch + 1) % args.lr_drop == 0 or (epoch + 1) % args.save_checkpoint_interval == 0:
-    #             checkpoint_paths.append(output_dir / f'checkpoint{epoch:04}.pth')
-    #         for checkpoint_path in checkpoint_paths:
-    #             weights = {
-    #                 'model': model_without_ddp.state_dict(),
-    #                 'optimizer': optimizer.state_dict(),
-    #                 'lr_scheduler': lr_scheduler.state_dict(),
-    #                 'epoch': epoch,
-    #                 'args': args,
-    #             }
-
-    #             utils.save_on_master(weights, checkpoint_path)
-                
-    #     # eval
-    #     test_stats, coco_evaluator = evaluate(
-    #         model, criterion, postprocessors, data_loader_val, base_ds, device, args.output_dir,
-    #         wo_class_error=wo_class_error, args=args, logger=(logger if args.save_log else None)
-    #     )
-    #     map_regular = test_stats['coco_eval_bbox'][0]
-    #     _isbest = best_map_holder.update(map_regular, epoch, is_ema=False)
-    #     if _isbest:
-    #         checkpoint_path = output_dir / 'checkpoint_best_regular.pth'
-    #         utils.save_on_master({
-    #             'model': model_without_ddp.state_dict(),
-    #             'optimizer': optimizer.state_dict(),
-    #             'lr_scheduler': lr_scheduler.state_dict(),
-    #             'epoch': epoch,
-    #             'args': args,
-    #         }, checkpoint_path)
-    #     log_stats = {
-    #         **{f'train_{k}': v for k, v in train_stats.items()},
-    #         **{f'test_{k}': v for k, v in test_stats.items()},
-    #     }
-
-
-    #     try:
-    #         log_stats.update({'now_time': str(datetime.datetime.now())})
-    #     except:
-    #         pass
-        
-    #     epoch_time = time.time() - epoch_start_time
-    #     epoch_time_str = str(datetime.timedelta(seconds=int(epoch_time)))
-    #     log_stats['epoch_time'] = epoch_time_str
-
+    #     log_stats = {**{f'test_{k}': v for k, v in test_stats.items()} }
     #     if args.output_dir and utils.is_main_process():
     #         with (output_dir / "log.txt").open("a") as f:
     #             f.write(json.dumps(log_stats) + "\n")
 
-    #         # for evaluation logs
-    #         if coco_evaluator is not None:
-    #             (output_dir / 'eval').mkdir(exist_ok=True)
-    #             if "bbox" in coco_evaluator.coco_eval:
-    #                 filenames = ['latest.pth']
-    #                 if epoch % 50 == 0:
-    #                     filenames.append(f'{epoch:03}.pth')
-    #                 for name in filenames:
-    #                     torch.save(coco_evaluator.coco_eval["bbox"].eval,
-    #                                output_dir / "eval" / name)
-    # total_time = time.time() - start_time
-    # total_time_str = str(datetime.timedelta(seconds=int(total_time)))
-    # print('Training time {}'.format(total_time_str))
-
-    # # remove the copied files.
-    # copyfilelist = vars(args).get('copyfilelist')
-    # if copyfilelist and args.local_rank == 0:
-    #     from datasets.data_util import remove
-    #     for filename in copyfilelist:
-    #         print("Removing: {}".format(filename))
-    #         remove(filename)
-
+    #     return
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('DETR training and evaluation script', parents=[get_args_parser()])
