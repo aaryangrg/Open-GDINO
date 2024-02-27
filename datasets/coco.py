7ad9c87d28379c2de2fd75bee27a515043a7eca9
Aaryan Garg <aaryan.garg1@gmail.com>
2024-02-27 10:51:22 +0530

    ft : non flexible training

--- datasets/coco.py
+++ datasets/coco.py
@@ -556,7 +556,7 @@ def make_coco_transforms(image_set, fix_size=False, strong_aug=False, args=None)
 
         # 480 x 360 roughly --> testing model performance
         return T.Compose([
-            T.RandomResize([1000,1333]),
+            T.RandomResize([(1000,1333)]),
             normalize,
         ])
 
